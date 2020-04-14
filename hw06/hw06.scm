c6c506f12b51ad23d8c39fd8e431c1e35594e1d4
Yin Qiu <qyinhelena@users.noreply.github.com>
2020-04-14 16:13:24 -0700

    hw06

---
+++ hw06/hw06.scm
@@ -0,0 +1,37 @@
+;;;;;;;;;;;;;;;
+;; Questions ;;
+;;;;;;;;;;;;;;;
+
+; Scheme
+
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  (car (cdr s))
+)
+
+(define (caddr s)
+  (car (cdr (cdr s)))
+)
+
+(define (sign x)
+  (cond
+      ((< x 0) -1)
+        ((= x 0) 0)
+        (else 1))
+)
+
+(define (square x) (* x x))
+
+(define (pow b n)
+  (cond
+      ((= n 0) 1)
+      ((even? n) (square (pow b (/ n 2))))
+      ((odd? n) (* b (square (pow b (/ (- n 1) 2))))))
+)
+
+(define (unique s)
+  (if (null? s) nil
+      (cons (car s) (filter (lambda (x) (not (eq? x (car s)))) (unique (cdr s)))))
+)
