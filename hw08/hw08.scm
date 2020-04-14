98e14d82687fa136d64b204d62b182129435c5e3
Yin Qiu <qyinhelena@users.noreply.github.com>
2020-04-14 16:15:55 -0700

    hw8

---
+++ hw08/hw08.scm
@@ -0,0 +1,6 @@
+; Macros
+
+(define-macro (list-of map-expr for var in lst if filter-expr)
+  list (list 'map (list 'lambda (list var) map-expr)
+    (list 'filter (list 'lambda (list var) filter-expr) lst))
+)
